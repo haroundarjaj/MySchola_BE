551c363403444da4f716aebca6379628b8269342
Haroun <darjaj.haroun@gmail.com>
2024-09-24 22:56:03 +0100

    create main files and include mapstruct mapping

---
+++ MySchola/src/main/java/com/dartech/myschola/MyScholaApplication.java
@@ -0,0 +1,14 @@
+package com.dartech.myschola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyScholaApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyScholaApplication.class, args);
+    }
+
+}
---
+++ MySchola/src/test/java/com/dartech/myschola/MyScholaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dartech.myschola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyScholaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
